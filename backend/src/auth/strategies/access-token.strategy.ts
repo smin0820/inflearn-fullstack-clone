d8553c4c76b3cd148de56935b940d4ecdc72efa6
smin0820 <97029953+smin0820@users.noreply.github.com>
2025-11-24 18:50:10 +0900

    ✨ feat : Auth Guard 추가 및 JWT Test Controller 추가

---
+++ backend/src/auth/strategies/access-token.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+type JwtPayload = {
+  sub: string;
+  email?: string;
+  name?: string;
+  picture?: null;
+  iat?: number;
+};
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-access-token',
+) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.AUTH_SECRET!,
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async validate(payload: JwtPayload) {
+    return payload;
+  }
+}
--- frontend/auth.ts
+++ frontend/auth.ts
@@ -1,8 +1,10 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "@/prisma"
-import CredentialsProvider from "next-auth/providers/credentials" 
-import { comparePassword } from "./lib/password-utils"
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { comparePassword } from "./lib/password-utils";
+import * as jwt from "jsonwebtoken";
+import { JWT } from "next-auth/jwt";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   useSecureCookies: process.env.NODE_ENV === "production",
@@ -11,21 +13,21 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   secret: process.env.AUTH_SECRET,
   providers: [
     CredentialsProvider({
-      name: 'credentials',
+      name: "credentials",
       credentials: {
         email: {
-          label: '이메일',
-          type: 'email',
-          placeholder: '이메일 입력',
+          label: "이메일",
+          type: "email",
+          placeholder: "이메일 입력",
         },
         password: {
-          label: '비밀번호',
-          type: 'password',
-        }
+          label: "비밀번호",
+          type: "password",
+        },
       },
       async authorize(credentials) {
         // 1. 모든 값이 정상적으로 들어왔는가?
-        if(!credentials || !credentials.email || !credentials.password) {
+        if (!credentials || !credentials.email || !credentials.password) {
           throw new Error("이메일과 비밀번호를 입력해주세요.");
         }
 
@@ -33,30 +35,38 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         const user = await prisma.user.findUnique({
           where: {
             email: credentials.email as string,
-          }
-        })
+          },
+        });
 
-        if(!user) {
+        if (!user) {
           throw new Error("존재하지 않는 이메일입니다.");
         }
 
         // 3. 비밀번호 일치 여부 확인
         const passwordMatch = comparePassword(
-          credentials.password as string, 
+          credentials.password as string,
           user.hashedPassword as string
         );
 
-        if(!passwordMatch) {
+        if (!passwordMatch) {
           throw new Error("비밀번호가 일치하지 않습니다.");
         }
 
         return user;
       },
-    })
+    }),
   ],
   session: {
-    strategy: 'jwt',
+    strategy: "jwt",
+  },
+  jwt: {
+    encode: async ({ token, secret }) => {
+      return jwt.sign(token as jwt.JwtPayload, secret as string);
+    },
+    decode: async ({ token, secret }) => {
+      return jwt.verify(token as string, secret as string) as JWT;
+    },
   },
   pages: {},
   callbacks: {},
-})
+});
